6b2a27def876a19caa19bbdbe8b8d29ecafd29e0
studysparkai29-arch <studysparkai29@gmail.com>
2026-03-29 00:10:51 +0530

    Create OneSignalSDKWorker.js

---
+++ OneSignalSDKWorker.js
@@ -0,0 +1,5 @@
+// OneSignal Service Worker
+// Yeh file GitHub pe index.html ke saath upload karni hai
+// File name: OneSignalSDKWorker.js (exactly this name)
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
+ 
